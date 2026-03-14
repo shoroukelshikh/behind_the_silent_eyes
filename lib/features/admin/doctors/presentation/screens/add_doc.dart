b90c6e063b7b3712278ab2122719b9cead65efc8
Salma Elshikh <salmaelshikh602@gmail.com>
2026-03-14 15:06:49 +0200

    + splash screen color changed

---
+++ lib/features/admin/doctors/presentation/screens/add_doc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddDoc extends StatelessWidget {
+  const AddDoc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
